7cc021390153fbe1f100ded35e33ffd63082581a
notifyq <122970800+notifyq@users.noreply.github.com>
2024-10-19 02:56:24 +0500

    Update AuthOptions.cs

--- api_cd/AuthOptions.cs
+++ api_cd/AuthOptions.cs
@@ -9,8 +9,8 @@ namespace api_CodeFlow
         public const string AUDIENCE = "https://localhost:44348"; // потребитель токена
         public const string KEY = "mysupersecretsecretkey!123mysupersecretsecretkey!123";   // ключ для шифрации
 
-        public const string MyEmail = "dmitriiyuzhakov@gmail.com";
-        public const string MyPassword = "yctl rtvb zrpv dtri";
+        public const string MyEmail = "";
+        public const string MyPassword = "";
         public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
     }
